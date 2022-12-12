e4f49565521431b0994a5c30367a5e0e0bf945f7
Thom van den Hil <tvdhil@datho.com>
2022-12-12 22:28:55 +0100

    partially implemented day 11

--- 2022/11/1.py
+++ 2022/11/1.py
@@ -1,15 +1,37 @@
+from dataclasses import dataclass
+from typing import List
+import re
+
 def getInputStr(inputFileName):
 	with open(inputFileName, 'r') as inputFile:
 		return inputFile.read()
 
 def getTransformedInput(inputFileName):
-	return getInputStr(inputFileName)
+	return getInputStr(inputFileName).split('\n' * 2)
+
+class Monkey:
+	inventory: List[int]
+	operationStr: str
+	testNum: int
+	targetMonkeyIndexIfTrue: int
+	targetMonkeyIndexIfFalse: int
 
 def main():
 	input = getTransformedInput('example_input.txt')
 	input = getTransformedInput('input.txt')
-	
+	monkeyDescriptions = input
 
+	receivedItems = dict()
+	inspectationCount = dict()
+	monkeyLst = []
+
+	for monkeyDescription in monkeyDescriptions:
+		inventory = re.search(r'Starting items: ([^\n]+)', monkeyDescription).group(1).split(',').map(int)
+
+	for _ in range(20):
+		# single round
+			
+			receivedItems[monkeyTarget] = []
 
 if __name__ == '__main__':
 	main()
