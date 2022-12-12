404102ab35f55d192c5c2f05577f6aebae949e2e
oThommy <tvdhil@datho.com>
2022-12-13 00:05:24 +0100

    solved day 11; added .vscode/ to .gitignore

--- 2022/11/1.py
+++ 2022/11/1.py
@@ -1,6 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import math
+from operator import attrgetter
 from typing import List
 import re
+
+
+# FIXME: remove
+from pprint import pprint
+
+import numpy as np
 
 def getInputStr(inputFileName):
 	with open(inputFileName, 'r') as inputFile:
@@ -9,29 +17,49 @@ def getInputStr(inputFileName):
 def getTransformedInput(inputFileName):
 	return getInputStr(inputFileName).split('\n' * 2)
 
+@dataclass
 class Monkey:
 	inventory: List[int]
 	operationStr: str
 	testNum: int
 	targetMonkeyIndexIfTrue: int
 	targetMonkeyIndexIfFalse: int
+	inspectedItemsAmount: int = field(init=False, default=0)
 
 def main():
 	input = getTransformedInput('example_input.txt')
 	input = getTransformedInput('input.txt')
 	monkeyDescriptions = input
 
-	receivedItems = dict()
-	inspectationCount = dict()
-	monkeyLst = []
+	monkeyLst: List[Monkey] = []
 
 	for monkeyDescription in monkeyDescriptions:
-		inventory = re.search(r'Starting items: ([^\n]+)', monkeyDescription).group(1).split(',').map(int)
+		monkeyData = {
+			'inventory': [int(item) for item in re.search(r'Starting items: ([^\n]+)', monkeyDescription).group(1).split(',')],
+			'operationStr': re.search(r'old .+(?=\n)', monkeyDescription).group(),
+			'testNum': int(re.search(r'divisible by (\d+)', monkeyDescription).group(1)),
+			'targetMonkeyIndexIfTrue': int(re.search(r'If true: throw to monkey (\d+)', monkeyDescription).group(1)),
+			'targetMonkeyIndexIfFalse': int(re.search(r'If false: throw to monkey (\d+)', monkeyDescription).group(1)),
+		}
+
+		monkeyLst.append(Monkey(**monkeyData))
 
 	for _ in range(20):
 		# single round
-			
-			receivedItems[monkeyTarget] = []
+		for monkey in monkeyLst:
+			while(monkey.inventory):
+				item = monkey.inventory.pop(0)
+				monkey.inspectedItemsAmount += 1
+				worryLevel = eval(monkey.operationStr, { 'old': item })
+				worryLevel = math.floor(worryLevel / 3)
+
+				if worryLevel % monkey.testNum == 0:
+					monkeyLst[monkey.targetMonkeyIndexIfTrue].inventory.append(worryLevel)
+				else:
+					monkeyLst[monkey.targetMonkeyIndexIfFalse].inventory.append(worryLevel)
+	
+	print(np.product(sorted(monkey.inspectedItemsAmount for monkey in monkeyLst)[-2:]))
+
 
 if __name__ == '__main__':
 	main()
--- 2022/11/2.py
+++ 2022/11/2.py
@@ -1,14 +1,60 @@
+from dataclasses import dataclass, field
+from typing import List
+import math
+import re
+
 def getInputStr(inputFileName):
 	with open(inputFileName, 'r') as inputFile:
 		return inputFile.read()
 
 def getTransformedInput(inputFileName):
-	return getInputStr(inputFileName)
+	return getInputStr(inputFileName).split('\n' * 2)
+
+@dataclass
+class Monkey:
+	inventory: List[int]
+	operationStr: str
+	testNum: int
+	targetMonkeyIndexIfTrue: int
+	targetMonkeyIndexIfFalse: int
+	inspectedItemsAmount: int = field(init=False, default=0)
 
 def main():
 	input = getTransformedInput('example_input.txt')
 	input = getTransformedInput('input.txt')
+	monkeyDescriptions = input
+
+	monkeyLst: List[Monkey] = []
+
+	for monkeyDescription in monkeyDescriptions:
+		monkeyData = {
+			'inventory': [int(item) for item in re.search(r'Starting items: ([^\n]+)', monkeyDescription).group(1).split(',')],
+			'operationStr': re.search(r'old .+(?=\n)', monkeyDescription).group(),
+			'testNum': int(re.search(r'divisible by (\d+)', monkeyDescription).group(1)),
+			'targetMonkeyIndexIfTrue': int(re.search(r'If true: throw to monkey (\d+)', monkeyDescription).group(1)),
+			'targetMonkeyIndexIfFalse': int(re.search(r'If false: throw to monkey (\d+)', monkeyDescription).group(1)),
+		}
+
+		monkeyLst.append(Monkey(**monkeyData))
+
+	testNumProduct = math.prod([monkey.testNum for monkey in monkeyLst])
+
+	for _ in range(10000):
+		# single round
+		for monkey in monkeyLst:
+			while(monkey.inventory):
+				item = monkey.inventory.pop(0)
+				monkey.inspectedItemsAmount += 1
+				worryLevel = eval(monkey.operationStr, { 'old': item })
+				# TODO: why does this work exactly?
+				worryLevel = worryLevel % testNumProduct
+
+				if worryLevel % monkey.testNum == 0:
+					monkeyLst[monkey.targetMonkeyIndexIfTrue].inventory.append(worryLevel)
+				else:
+					monkeyLst[monkey.targetMonkeyIndexIfFalse].inventory.append(worryLevel)
 	
+	print(math.prod(sorted(monkey.inspectedItemsAmount for monkey in monkeyLst)[-2:]))
 
 
 if __name__ == '__main__':
